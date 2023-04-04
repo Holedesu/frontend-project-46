938d206f35b2c0e11a1da75ef59a63555c35c053
Holedesu <88774183+Holedesu@users.noreply.github.com>
2023-04-04 12:48:29 +0300

    Ended second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,9 @@ import { program } from 'commander';
 
 program
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0', '-V, --version', 'output the version number');
+    .version('1.0.0', '-V, --version', 'output the version number')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format <type>', 'output format')
 
 program.parse();
 
