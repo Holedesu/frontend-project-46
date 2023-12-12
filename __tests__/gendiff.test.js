b7aa637addd540f01b00294c551c7175c8b7ea09
Holedesu <88774183+Holedesu@users.noreply.github.com>
2023-12-12 18:55:52 +0300

    fixed linter

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -13,48 +13,48 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('tree structures', () => {
-    test('json type', () => {
-        const file1 = parser(readFile('file1.json'), 'json');
-        const file2 = parser(readFile('file2.json'), 'json');
-        expect(getDifference(file1, file2)).toBeInstanceOf(Object);
-    });
-    test('yaml/yml type', () => {
-        const file1 = parser(readFile('file1.yaml'), 'yaml');
-        const file2 = parser(readFile('file2.yaml'), 'yaml');
-        expect(getDifference(file1, file2)).toBeInstanceOf(Object);
-    });
+  test('json type', () => {
+    const file1 = parser(readFile('file1.json'), 'json');
+    const file2 = parser(readFile('file2.json'), 'json');
+    expect(getDifference(file1, file2)).toBeInstanceOf(Object);
+  });
+  test('yaml/yml type', () => {
+    const file1 = parser(readFile('file1.yaml'), 'yaml');
+    const file2 = parser(readFile('file2.yaml'), 'yaml');
+    expect(getDifference(file1, file2)).toBeInstanceOf(Object);
+  });
 });
 
 describe('formaters', () => {
-    test('stylish type', () => {
-        const file1 = parser(readFile('file1.json'), 'json');
-        const file2 = parser(readFile('file2.json'), 'json');
-        const reference = readFile('stylish.txt');
-        expect(doFormating(getDifference(file1, file2), 'stylish')).toEqual(reference);
-    });
+  test('stylish type', () => {
+    const file1 = parser(readFile('file1.json'), 'json');
+    const file2 = parser(readFile('file2.json'), 'json');
+    const reference = readFile('stylish.txt');
+    expect(doFormating(getDifference(file1, file2), 'stylish')).toEqual(reference);
+  });
 
-    test('plain type', () => {
-        const file1 = parser(readFile('file1.json'), 'json');
-        const file2 = parser(readFile('file2.json'), 'json');
-        const reference = readFile('plain.txt');
-        expect(doFormating(getDifference(file1, file2), 'plain')).toEqual(reference);
-    });
+  test('plain type', () => {
+    const file1 = parser(readFile('file1.json'), 'json');
+    const file2 = parser(readFile('file2.json'), 'json');
+    const reference = readFile('plain.txt');
+    expect(doFormating(getDifference(file1, file2), 'plain')).toEqual(reference);
+  });
 
-    test('json type', () => {
-        const file1 = parser(readFile('file1.json'), 'json');
-        const file2 = parser(readFile('file2.json'), 'json');
-        const reference = readFile('json.txt');
-        expect(doFormating(getDifference(file1, file2), 'json')).toEqual(reference);
-    });
+  test('json type', () => {
+    const file1 = parser(readFile('file1.json'), 'json');
+    const file2 = parser(readFile('file2.json'), 'json');
+    const reference = readFile('json.txt');
+    expect(doFormating(getDifference(file1, file2), 'json')).toEqual(reference);
+  });
 });
 
 describe('parser', () => {
-    test('Have property', () => {
-        const json = readFile('file1.json');
-        expect(parser(json, 'json')).toHaveProperty('common.setting2', 200);
-    });
+  test('Have property', () => {
+    const json = readFile('file1.json');
+    expect(parser(json, 'json')).toHaveProperty('common.setting2', 200);
+  });
 
-    test('Not supported format', () => {
-        expect(() => parser('txt')).toThrow(Error);
-    });
+  test('Not supported format', () => {
+    expect(() => parser('txt')).toThrow(Error);
+  });
 });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,12 +4,12 @@ import { program } from 'commander';
 import calcDiff from '../index.js';
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .option('-f, --format <type>', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => {
-        console.log(calcDiff(filepath1, filepath2, program.opts().format));
-    });
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(calcDiff(filepath1, filepath2, program.opts().format));
+  });
 
 program.parse();
